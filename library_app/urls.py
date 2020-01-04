40d41417af190cf2cd6738ec5d59ac6f9f576fe2
Andrew Porritt <aporritt@and.digital>
2020-01-04 19:04:45 +0000

    further additions to user.is_authenticated

--- library_app/urls.py
+++ library_app/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import HomePageView, LibraryPageView, logout, login
+from .views import HomePageView, LibraryPageView, logout, login, signup
 
 urlpatterns = [
     path('', HomePageView.as_view(), name='home'),
     path('library_view/', LibraryPageView.as_view(), name='library_view'),
     path('logout/' , logout, name='logout'),
-    path('login/' , login , name='login')
+    path('login/' , login , name='login'),
+    path('signup/' , signup, name='signup')
 ]
--- library_app/views.py
+++ library_app/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect, reverse
 from django.contrib import auth, messages
 from django.views.generic import TemplateView
+from django.contrib.auth.decorators import login_required
 from .forms import LibraryLoginForm
 
 # Create your views here.
@@ -13,6 +14,7 @@ class HomePageView(TemplateView):
 class LibraryPageView(TemplateView):
     template_name = 'library_view.html'
 
+@login_required
 def logout(request):
     """ user logout """
     auth.logout(request)
@@ -21,6 +23,9 @@ def logout(request):
 
 def login(request):
     """ login func """
+    if request.user.is_authenticated:
+        return redirect(reverse('home'))
+
     if request.method == "POST":
         login_form = LibraryLoginForm(request.POST)
 
@@ -32,9 +37,13 @@ def login(request):
             if user:
                 auth.login(user=user, request=request)
                 messages.success(request, "You have successfully logged into the Library")
+                return redirect(reverse('home'))
             else: 
                 login_form.add_error(None, "Sorry, some of your details are incorrect. Please try again")
 
     else:
         login_form = LibraryLoginForm()
     return render(request, 'login.html', {'login_form': login_form})
+
+def signup(request):
+    return render(request, 'signup.html')
