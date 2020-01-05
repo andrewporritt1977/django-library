7d045354f598f9280d436910b802e8c1941776f9
Andrew Porritt <aporritt@and.digital>
2020-01-05 15:47:31 +0000

    hardcoded secret key, email and pass to remove dotenv dependency. in heoku test deploy. will use Fabric for full deploy.

--- library_project/settings.py
+++ library_project/settings.py
@@ -11,9 +11,9 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
-from dotenv import load_dotenv
+"""from dotenv import load_dotenv
 
-load_dotenv()
+load_dotenv()"""
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv('SECRET_KEY')
+SECRET_KEY = '*ylp@%6+lg@c+o#zmp3u#hyi^dug36vpg%xsc5_pw5hx0q7jpj'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 
@@ -143,8 +143,8 @@ MESSAGE_STORAGE = 'django.contrib.messages.storage.session.SessionStorage'
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = os.getenv("EMAIL_ADDRESS")
-EMAIL_HOST_PASSWORD = os.getenv("EMAIL_PASSWORD")
+EMAIL_HOST_USER = 'django.library.app.com'
+EMAIL_HOST_PASSWORD = 'Barros99'
 EMAIL_PORT = 587
 
-DEBUG = os.getenv('DEBUG')
+DEBUG = True
