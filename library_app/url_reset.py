f4ace272c28c772011978668fff542179dcef05f
Andrew Porritt <aporritt@and.digital>
2020-01-08 23:39:15 +0000

    tidied password reset directories

--- library_app/url_reset.py
+++ library_app/url_reset.py
@@ -1,12 +1,13 @@
 from django.conf.urls import url
 from django.urls import reverse_lazy
-from django.contrib.auth.views import PasswordResetView, PasswordResetConfirmView, PasswordResetCompleteView
-from .views import password_reset_done
+
+from django.contrib.auth import views as auth_views
+
 
 urlpatterns = [
-    url(r'^$', PasswordResetView.as_view(), name='password_reset'),
-    url(r'^done/$', password_reset_done, name='password_reset_done'),
-    url(r'^(?P<uidb64>[0-9A-Za-z]+)-(?P<token>.+)/$', PasswordResetConfirmView.as_view(),
+    url(r'^$', auth_views.password_reset, name='password_reset'),
+    url(r'^done/$', auth_view.password_reset_done, name='password_reset_done'),
+    url(r'^(?P<uidb64>[0-9A-Za-z]+)-(?P<token>.+)/$', auth_views.password_reset_confirm,
         {'post_reset_confirm' : reverse_lazy('password_reset_complete')}, name='password_reset_confirm'),
-    url(r'^complete/$', PasswordResetCompleteView.as_view() , name='password_reset_complete')
+    url(r'^complete/$', auth_views.password_reset_complete , name='password_reset_complete')
 ]
