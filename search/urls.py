532ca37b8faf7b9c16989924bd073079370da693
Andrew Porritt <aporritt@and.digital>
2020-01-14 23:16:49 +0000

    fixed reset view

--- search/urls.py
+++ search/urls.py
@@ -1,8 +1,8 @@
 from django.conf.urls import url
-from .views import book_search, genre_filter
+from .views import book_search, genre_filter, reset_view
 
 urlpatterns = [
     url(r'^$', book_search, name='search'),
-    url(r'^filt$', genre_filter, name='filter')
-
+    url(r'^filt$', genre_filter, name='filter'),
+    url(r'^reset$', reset_view, name='reset_view')
 ]
--- search/views.py
+++ search/views.py
@@ -9,3 +9,7 @@ def book_search(request):
 def genre_filter(request):
     books = Book.objects.filter(genre__icontains=request.GET['gFilter'])
     return render(request, "books.html", {"books" : books})
+
+def reset_view(request):
+    books = books = Book.objects.all().order_by("title")
+    return render(request, "books.html", {"books" : books})
