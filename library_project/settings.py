9b56d5270c7122884972f6a997a0b469befa33d1
Andrew Porritt <aporritt@and.digital>
2020-01-15 23:06:24 +0000

    added static root to settings.py

--- library_project/settings.py
+++ library_project/settings.py
@@ -165,3 +165,4 @@ STRIPE_PUBLISHABLE = os.getenv('STRIPE_PUBLISHABLE')
 STRIPE_SECRET = os.getenv('STRIPE_SECRET')
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
