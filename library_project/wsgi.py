75cd8ddd2117d187f868d76ec4b93f4a88828a05
Andrew Porritt <aporritt@and.digital>
2020-01-15 16:13:17 +0000

    removed dotenv reference form WSGI.py

--- library_project/wsgi.py
+++ library_project/wsgi.py
@@ -8,11 +8,11 @@ https://docs.djangoproject.com/en/3.0/howto/deployment/wsgi/
 """
 
 import os
-from dotenv import load_dotenv
+
 from django.core.wsgi import get_wsgi_application
 
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'library_project.settings')
-load_dotenv(os.path.join('/Users/andrewporritt/python-django/library_project', '.env'))
+
 
 application = get_wsgi_application()
