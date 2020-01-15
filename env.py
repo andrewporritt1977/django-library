e796d265c7bfeab0c511ed5901861d0eecc49f65
Andrew Porritt <aporritt@and.digital>
2020-01-15 00:20:01 +0000

    migrated enviromment varibles. set up gitignore.

--- env.py
+++ env.py
@@ -1,6 +1,8 @@
-SECRET_KEY='*ylp@%6+lg@c+o#zmp3u#hyi^dug36vpg%xsc5_pw5hx0q7jpj'
-DEBUG=True
-EMAIL_ADDRESS='django.library.app@gmail.com'
-EMAIL_PASSWORD='Barros99'
-STRIPE_PUBLISHABLE='pk_test_GBdOM2ifiq3zR065LdokVedS00uUaAEmvf'
-STRIPE_SECRET='sk_test_oEgHeQRcU9fM8gto5qRg8HQL0044lUZsRv'
+import os
+
+os.environ.setdefault('SECRET_KEY', '*ylp@%6+lg@c+o#zmp3u#hyi^dug36vpg%xsc5_pw5hx0q7jpj')
+os.environ.setdefault('DEBUG', 'True')
+os.environ.setdefault('EMAIL_ADDRESS', 'django.library.app@gmail.com')
+os.environ.setdefault('EMAIL_PASSWORD', 'Barros99')
+os.environ.setdefault('STRIPE_PUBLISHABLE', 'pk_test_GBdOM2ifiq3zR065LdokVedS00uUaAEmvf')
+os.environ.setdefault('STRIPE_SECRET', 'sk_test_oEgHeQRcU9fM8gto5qRg8HQL0044lUZsRv')
