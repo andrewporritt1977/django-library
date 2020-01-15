64250a14b0838ae38ee332ef73d97a7f8d1b12e8
Andrew Porritt <aporritt@and.digital>
2020-01-15 16:38:56 +0000

    added heroku url to Allowed Hosts

--- library_project/settings.py
+++ library_project/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['*', 'https://warm-forest-68273.herokuapp.com/']
 
 
 # Application definition
