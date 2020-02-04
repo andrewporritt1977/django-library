d04af54cbd07d26649a8c948429c776dfc7aabbb
Andrew Porritt <aporritt@and.digital>
2020-02-04 23:49:05 +0000

    nav moved to footer js implemented for pop up media queries used to ensure nav is useable on smaller screens

---
+++ library_app/static/js/main.js
@@ -0,0 +1,11 @@
+// nav functions
+
+function navDown() {
+    document.getElementById("popNav").style.height= "0%";
+    document.getElementById("navLink").style.width= "0%";
+}
+
+function navUp() {
+    document.getElementById("popNav").style.height = "45%";
+    document.getElementById("navLink").style.width= "100%";
+}
