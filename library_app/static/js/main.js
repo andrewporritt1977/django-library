6c89ee790fb892baa5b307800886146a005db9cc
Andrew Porritt <aporritt@and.digital>
2020-02-05 16:25:30 +0000

    addressed bug in nav bar button via z-index.

--- library_app/static/js/main.js
+++ library_app/static/js/main.js
@@ -1,11 +1,18 @@
 // nav functions
 
-function navDown() {
-    document.getElementById("popNav").style.height= "0%";
-    document.getElementById("navLink").style.width= "0%";
-}
+// function navDown() {
+//     document.getElementById("popNav").style.height= "0%";
+//     document.getElementById("navLink").style.width= "0%";
+// }
 
 function navUp() {
-    document.getElementById("popNav").style.height = "45%";
-    document.getElementById("navLink").style.width= "100%";
+
+    var navState = document.getElementById("popNav")
+    if (navState.style.height == "0%") {
+        navState.style.height = "45%";
+    } else {
+        navState.style.height = "0%"
+    }
+    // document.getElementById("popNav").style.height = "45%";
+    // document.getElementById("navLink").style.width= "100%";
 }
