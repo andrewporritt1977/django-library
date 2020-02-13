a75aaf94a0aa1775962fb3a040d34f3f3a036b35
Andrew Porritt <aporritt@and.digital>
2020-02-13 23:10:55 +0000

    moved search / filter to footer. added icon and toggle

--- library_app/static/js/main.js
+++ library_app/static/js/main.js
@@ -4,16 +4,33 @@
 function navUp() {
 
     var navState = document.getElementById("popNav")
+    var searchState = document.getElementById("popSearch")
+    var hideSearchIcon = document.getElementById("searchicon")
+    
     if (navState.style.height == "0%" || navState.style.height == "") {
         navState.style.height = "45%";
+        searchState.style.height = "0%";
+        hideSearchIcon.style.display = "none";
     } else {
         navState.style.height = "0%";
+        hideSearchIcon.style.display = "block";
     }
 
 }
 
-function iconSpin() {
 
-    var navIcon = document.getElementById("addBookIcon")
-    
-}   
+// search filter toggle
+function searchUp() {
+
+    var searchState = document.getElementById("popSearch")
+    var navState = document.getElementById("popNav")
+    var hideNavIcon = document.getElementById("navicon")
+    if (searchState.style.height == "0%" || searchState.style.height == "") {
+        searchState.style.height = "45%";
+        navState.style.height = "0%";
+        hideNavIcon.style.display = "none";
+    } else {
+        searchState.style.height = "0%";
+        hideNavIcon.style.display = "none";
+    }
+}
