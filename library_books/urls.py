5297925dacba31c056e13263e2160ffcfef2937e
Andrew Porritt <aporritt@and.digital>
2020-02-18 23:35:42 +0000

    set debug to false for deploy

--- library_books/urls.py
+++ library_books/urls.py
@@ -4,6 +4,6 @@ from .views import all_books, check_out, add_book
 
 urlpatterns = [
     url(r'^$', all_books, name='books'),
-    url(r'^/add/$', add_book, name="add_book"),
+    url(r'^add/$', add_book, name="add_book"),
     url(r'^(?P<pk>\d+)/$', check_out, name='check_out')
 ]
