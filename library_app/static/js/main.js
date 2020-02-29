70d8308885fc43fa0155e30b739c13da161ff74a
Andrew Porritt <aporritt@and.digital>
2020-02-29 00:53:28 +0000

    swapped add book and search/filter buttons for better ux/ui

--- library_app/static/js/main.js
+++ library_app/static/js/main.js
@@ -1,34 +1,38 @@
 // nav functions
 
 // nav menu toggle
+// function also hides add book icon
 function navUp() {
 
     var navState = document.getElementById("popNav")
     var searchState = document.getElementById("popSearch")
-    var hideSearchIcon = document.getElementById("searchicon")
+    var hideAddIcon = document.getElementById("addBookIcon")
     
     if (navState.style.height == "0%" || navState.style.height == "") {
         navState.style.height = "45%";
         searchState.style.height = "0%";
-        hideSearchIcon.style.display = "none";
+        hideAddIcon.style.display = "none";
     } else {
         navState.style.height = "0%";
-        hideSearchIcon.style.display = "block";
+        hideAddIcon.style.display = "block";
     }
 
 }
 
 
 // search filter toggle
+// function also hides add book icon
 function searchUp() {
 
     var searchState = document.getElementById("popSearch")
     var navState = document.getElementById("popNav")
-    var hideNavIcon = document.getElementById("navicon")
+    var hideAddIcon = document.getElementById("addBookIcon")
     if (searchState.style.height == "0%" || searchState.style.height == "") {
         searchState.style.height = "45%";
         navState.style.height = "0%";
+        hideAddIcon.style.display = "none";
     } else {
         searchState.style.height = "0%";
+        hideAddIcon.style.display = "block";
     }
 }
